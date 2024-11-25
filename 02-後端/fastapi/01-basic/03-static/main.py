3b6ebb766ae1612ad951025c8e3fca5d1c040dae
ccckmit <ccckmit@gmail.com>
2024-11-25 10:28:17 +0800

    modify static/redirect

--- 02-後端/fastapi/01-basic/03-static/main.py
+++ 02-後端/fastapi/01-basic/03-static/main.py
@@ -1,16 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-import uvicorn
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 # 將 "static" 資料夾掛載為靜態檔案伺服目錄
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
+# 根路徑重定向到 static/index.html
 @app.get("/")
-async def read_root():
-    return {
-        "message": "Welcome! Access static files at /static/<filename>",
-        "example": "http://127.0.0.1:8000/static/README.md"
-    }
+async def root():
+    return RedirectResponse(url="/static/index.html")
 
---
+++ 02-後端/fastapi/README.md
@@ -0,0 +1,5 @@
+
+
+* [在 fastapi 中如何使用 Cython](https://chatgpt.com/c/6743c3ec-fb70-8012-b7d5-8ca806c9e643)
+
+* [fastapi 的程式可以不要用 typed annotation 還能跑嗎？](https://chatgpt.com/c/6743de52-fb40-8012-81b0-d3cb6a4421a5)
